e5c94972ed97bdb7e7f20a8fdebe1cf35b551f8c
mutao-net <sasaki.h1026@gmail.com>
2022-03-06 14:04:40 +0900

    s3

--- s3/main.tf
+++ s3/main.tf
@@ -1,10 +1,13 @@
-resource "aws_s3_bucket" "bucket-sample" {
+resource "aws_s3_bucket" "private" {
   bucket = "mutao-sample-bucket"
-  acl    = "private"
-  lifecycle_rule {
-    enabled = true
-    expiration {
-      days = "180"
+  versioning {
+    enabled = false
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
 }
