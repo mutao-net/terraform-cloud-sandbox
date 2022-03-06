ddf25678819f27ebfde21a66dd8775142ddd546e
mutao-net <sasaki.h1026@gmail.com>
2022-03-06 14:02:34 +0900

    delete s3

--- s3/main.tf
+++ s3/main.tf
@@ -1,19 +1,6 @@
-resource "aws_s3_bucket" "private" {
-  bucket = "mutao-tf-bucket"
-  versioning {
-    enabled = false
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_s3_bucket" "bucket-sample" {
   bucket = "mutao-sample-bucket"
+  acl    = "private"
   lifecycle_rule {
     enabled = true
     expiration {
